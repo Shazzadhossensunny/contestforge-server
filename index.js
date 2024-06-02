72209b0d6b38b72e8847ef968c9bcf05b8ee1f16
Shazzadhossensunny <shazzadhossensunny@gmail.com>
2024-06-03 03:32:35 +0600

    user get psot patch api crate

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+     const usersCollection = client.db('contestForgeDB').collection('users')
      const contestCollection = client.db('contestForgeDB').collection('contests')
 
       // jwt api
@@ -57,25 +58,67 @@ async function run() {
     };
 
       // use verify admin after verify token
-      // const verifyAdmin = async (req, res, next) => {
-      //   const email = req.decoded.email;
-      //   const query = { email: email };
-      //   const user = await userCollection.findOne(query);
-      //   const isAdmin = user?.role === "admin";
-      //   if (!isAdmin) {
-      //     return res.status(403).send({ message: "forbidden access" });
-      //   }
-      //   next();
-      // };
+      const verifyAdmin = async (req, res, next) => {
+        const email = req.decoded.email;
+        const query = { email: email };
+        const user = await usersCollection.findOne(query);
+        const isAdmin = user?.role === "admin";
+        if (!isAdmin) {
+          return res.status(403).send({ message: "forbidden access" });
+        }
+        next();
+      };
+      // use verify contest creator after verify token
+      const verifyCreator = async (req, res, next) => {
+        const email = req.decoded.email;
+        const query = { email: email };
+        const user = await usersCollection.findOne(query);
+        const isCreator = user?.role === "creator";
+        if (!isCreator) {
+          return res.status(403).send({ message: "forbidden access" });
+        }
+        next();
+      };
+
+      // users api
+      app.get("/users", async(req, res)=> {
+        const result = await usersCollection.find().toArray()
+        res.send(result);
+      })
+      app.post("/users", async (req, res) => {
+        const user = req.body;
+        const query = { email: user.email };
+        const existingUser = await usersCollection.findOne(query);
+        if (existingUser) {
+          return res.send({ message: "user already exists", insertedId: null });
+        }
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+      });
+
+      app.patch("/user/:id", async(req, res)=>{
+        const id = req.params.id;
+        const {role} = req.body;
+        const filter = {_id: new ObjectId(id)}
+        const updateDoc = {
+          $set:{
+            role :role
+          }
+        }
+        const result = await usersCollection.updateOne(filter, updateDoc)
+        res.send(result)
+
+      })
+
+      app.delete("/users/:id", async(req, res)=> {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await usersCollection.deleteOne(query)
+        res.send(result)
+      })
+
 
     //  contest api
-
-    app.post("/addContest", async(req, res)=>{
-      const contest = req.body;
-      const result = await contestCollection.insertOne(contest)
-      res.send(result)
-    })
-
     app.get("/addContest", async(req,res)=>{
       const result = await contestCollection.find().toArray()
       res.send(result)
@@ -89,6 +132,14 @@ async function run() {
       res.send(result)
 
     })
+
+    app.post("/addContest", async(req, res)=>{
+      const contest = req.body;
+      const result = await contestCollection.insertOne(contest)
+      res.send(result)
+    })
+
+
 
 
 
