6a32e4cfa0297d01f723584c9105f90f77d2d1e9
Shazzadhossensunny <shazzadhossensunny@gmail.com>
2024-06-02 15:50:29 +0600

    add contest api create

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 var cors = require("cors");
+var jwt = require("jsonwebtoken");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
@@ -28,6 +29,60 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+     const contestCollection = client.db('contestForgeDB').collection('contests')
+
+      // jwt api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "7d",
+      });
+      res.send({ token });
+    });
+
+     // middleware
+     const verifyToken = (req, res, next) => {
+      // console.log("inside verify token", req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "unauthorized access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
+      // use verify admin after verify token
+      // const verifyAdmin = async (req, res, next) => {
+      //   const email = req.decoded.email;
+      //   const query = { email: email };
+      //   const user = await userCollection.findOne(query);
+      //   const isAdmin = user?.role === "admin";
+      //   if (!isAdmin) {
+      //     return res.status(403).send({ message: "forbidden access" });
+      //   }
+      //   next();
+      // };
+
+    //  contest api
+    app.post("/addContest", async(req, res)=>{
+      const contest = req.body;
+      const result = await contestCollection.insertOne(contest)
+      res.send(result)
+    })
+
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
